0a635c307b31b85d6c2d6cb388c6d7ca3dde4249
Kiwii Vanya <kiwiivanya128@gmail.com>
2023-07-26 15:58:38 +0300

    fixed unloadable footer

--- js/footer_generator.js
+++ js/footer_generator.js
@@ -1,6 +1,20 @@
 // Function to fetch the external footer template
+
+function getCountToRoot() {
+  let path = window.location.pathname;
+
+  const slashCount = (path.match(/\//g) || []).length;
+  const splitedStr = path.split("CharacterRegister")[0];
+  const slashCountDomain = (splitedStr.match(/\//g) || []).length;
+  const resCount = slashCount - slashCountDomain - 1;
+
+  return resCount;
+}
+
+console.log(getCountToRoot());
+
 function fetchFooterTemplate() {
-  return fetch('./footer_template.html') // Change 'footer_template.html' to the correct path of your external footer file
+  return fetch(getCountToRoot() === 0? './pages/footer_template.html':'./footer_template.html') // Change 'footer_template.html' to the correct path of your external footer file
     .then(response => response.text())
     .catch(error => {
       console.error('Error fetching footer template:', error);
