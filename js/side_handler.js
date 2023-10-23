c04d2fc870874844301af1c3423a377dfc5acaeb
Kiwii Vanya <kiwiivanya128@gmail.com>
2023-10-23 19:44:03 +0300

    made a style changes for mobile devices

--- js/side_handler.js
+++ js/side_handler.js
@@ -3,14 +3,31 @@ var content = document.getElementById('content');
 const toggleButton = document.getElementById('toggleButton');
 
 function toggleSideMenu() {
+  const windowRatio = window.innerWidth / window.innerHeight;
+
+  if(windowRatio > 1.0){
+    sideMenu.style.width = "15vw";
+    sideMenu.style.height = "100%";
   if (sideMenu.style.display === 'none') {
     sideMenu.style.display = 'block';
-    content.style.marginLeft = '1.5vw';
+    content.style.marginLeft = '1.5vh';
   } else {
     sideMenu.style.display = 'none';
-    content.style.marginLeft = '1.5vw';
+    content.style.marginLeft = '1.5vh';
+  }} else {
+    sideMenu.style.width = "100%";
+    sideMenu.style.height = "100vh";
+    
+    if (sideMenu.style.display === 'none') {
+      sideMenu.style.display = 'block';
+      content.style.marginTop = '1.5vh';
+    } else {
+      sideMenu.style.display = 'none';
+      content.style.marginTop = '1.5vh';
+    } 
   }
-}
 
+}
+sideMenu.style.display = 'none'
 toggleButton.onclick = toggleSideMenu;
 
