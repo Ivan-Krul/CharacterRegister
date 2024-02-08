1fc2af150813a4c22fb10d7538fce96fc9ba233b
Ivan-Krul <kiwiivanya128@gmail.com>
2024-02-08 14:26:43 +0100

    updated style and get rid of circulator.gif

--- js/side_handler.js
+++ js/side_handler.js
@@ -6,24 +6,20 @@ function toggleSideMenu() {
   const windowRatio = window.innerWidth / window.innerHeight;
 
   if(windowRatio > 1.0){
-    sideMenu.style.width = "15vw";
+    sideMenu.style.width = "30vw";
     sideMenu.style.height = "100%";
   if (sideMenu.style.display === 'none') {
     sideMenu.style.display = 'block';
-    content.style.marginLeft = '1.5vh';
   } else {
     sideMenu.style.display = 'none';
-    content.style.marginLeft = '1.5vh';
   }} else {
     sideMenu.style.width = "100%";
     sideMenu.style.height = "100vh";
     
     if (sideMenu.style.display === 'none') {
       sideMenu.style.display = 'block';
-      content.style.marginTop = '1.5vh';
     } else {
       sideMenu.style.display = 'none';
-      content.style.marginTop = '1.5vh';
     } 
   }
 
