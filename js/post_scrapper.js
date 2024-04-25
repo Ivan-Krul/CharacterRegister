8ad2c1befeee32a9d4a8898d41650af0d2f1d7c9
Ivan-Krul <kiwiivanya128@gmail.com>
2024-04-25 22:13:09 +0200

    moved post_sc rapper scripts into separated file

---
+++ js/post_scrapper.js
@@ -0,0 +1,43 @@
+import * as fileFetcher from "../js/file_fetcher.js";
+        import * as postGenerator from "./../js/post_generator.js";
+        import * as videoException from "./../js/video_exception.js";
+
+        (async () => {
+            const urlParams = new URLSearchParams(window.location.search);
+            const post = urlParams.get('post');
+
+            if (post !== null) {
+                if (post !== "") {
+                    let page = "";
+                    try {
+                        let file = await fileFetcher.fetchFile("posts/" + post);
+                        page = postGenerator.printPostPage(file, post);
+                        if (page.split("Eek").length === 1) {
+                            let resPage = page.replaceAll("./", "./../");
+
+                            document.getElementById("post").innerHTML = resPage;
+                        }
+                        else
+                            throw "[Eek]";
+                    }
+                    catch (error) {
+                        document.getElementById("post").appendChild(await videoException.getExceptionNode(error));
+                    }
+                }
+                else {
+                    document.getElementById("post").innerHTML = "[allocated]<br/><video src=\"../resource/waw.mov\" autoplay controls></video>";
+                }
+            }
+            else {
+                document.getElementById("header").innerText += " Browser";
+                let partitions = await fileFetcher.fetchFile("posts/partitions.txt");
+                let partitionsArray = partitions.split("\n");
+
+                partitionsArray.forEach(element => {
+                    let el = document.createElement("a");
+                    el.href = "post_scrapper.html?post=" + element;
+                    el.innerText = "post_scrapper.html?post=" + element;
+                    document.getElementById("post").appendChild(el).appendChild(document.createElement("br"));
+                });
+            }
+        })();
