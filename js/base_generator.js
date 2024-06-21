f2d246f170633b63d9a5b0fa572a43957b876497
Ivan-Krul <kiwiivanya128@gmail.com>
2024-06-22 00:13:46 +0200

    fixed some generators and added empty placeholders

--- js/base_generator.js
+++ js/base_generator.js
@@ -7,7 +7,7 @@ async function assemble() {
   let side = await fileFetcher.fetchFile("pages/side_template.html");
 
     side = side.replace("./pages/post_scrapper.html",fileFetcher.makeLinkIndependent("./pages/post_scrapper.html"));
-    side = side.replace("./pages/about.html",fileFetcher.makeLinkIndependent("./pages/about.html"));
+    side = side.replace("./pages/commission.html",fileFetcher.makeLinkIndependent("./pages/commission.html"));
     side = side.replace("./index.html",fileFetcher.makeLinkIndependent("./index.html"));
 
   let doc = await fileFetcher.fetchFile("pages/footer_template.html");
