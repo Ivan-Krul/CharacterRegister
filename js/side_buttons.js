f03c0f38669350de69ed21d5da2d4a3ac2925ec6
Ivan-Krul <kiwiivanya128@gmail.com>
2024-11-17 16:36:56 +0100

    updated style of side buttons, so they are images

--- js/side_buttons.js
+++ js/side_buttons.js
@@ -1,110 +1,12 @@
 import * as fileFetcher from "./file_fetcher.js";
 
-/*
-
-function convertToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-  return "#" + convertToHex(r) + convertToHex(g) + convertToHex(b);
-}
-
-var index = 0;
-
-function animateTransfering(tagBody, directionToDark = true) {
-  setTimeout(() => {
-    if (directionToDark) {
-      tagBody.style.backgroundColor = rgbToHex(255 - index, 255 - index, 255 - index);
-      tagBody.style.color = rgbToHex(index, index, index);
-    }
-    else {
-      tagBody.style.color = rgbToHex(255 - index, 255 - index, 255 - index);
-      tagBody.style.backgroundColor = rgbToHex(index, index, index);
-    }
-    index+=step;
-    if (index <= 255)
-      animateTransfering(tagBody, directionToDark);
-    else
-      index = 0;
-  }, 1)
-}
-
-function changeBorderColor(targetDark = true) {
-  let images = document.querySelectorAll("img");
-  images.forEach(element => {
-    if(targetDark)
-      element.style.border = "5px solid white";
-    else 
-      element.style.border = "5px solid black";
-  });
-}
-
-function setThemeStyle(first = false) {
-  let isDarkTheme = true;
-  if (localStorage.getItem("isDarkTheme") === undefined) {
-    localStorage.setItem("isDarkTheme", isDarkTheme);
-  }
-  else {
-    let tagBody = document.body;
-    if (localStorage.getItem("isDarkTheme") === "true") {
-      if (!first) {
-        animateTransfering(tagBody, true);
-        changeBorderColor(isDarkTheme);
-      }
-      else {
-        tagBody.style.backgroundColor = "black";
-        tagBody.style.color = "white";
-        changeBorderColor(true);
-      }
-    }
-    else {
-      if (!first) {
-        animateTransfering(tagBody, false);
-        changeBorderColor(!isDarkTheme);
-      }
-      else {
-        tagBody.style.backgroundColor = "white";
-        tagBody.style.color = "black";
-        changeBorderColor(false);
-      }
-    }
-  }
-}
-
-export function handleClick() {
-  if(index)
-    return;
-
-  if (localStorage.getItem("isDarkTheme") === "true")
-    localStorage.setItem("isDarkTheme", "false");
-  else
-    localStorage.setItem("isDarkTheme", "true");
-  setThemeStyle();
-}
-
-function generateButtonToSwitch() {
-  let tagButton = document.getElementById("toggleTheme");
-  if(tagButton === null)
-   tagButton = document.getElementById("button_toggleTheme");
-  tagButton.onclick = handleClick;
-}
-
-setThemeStyle(true);
-if (fileFetcher.getURLParams().get("windowed") === null) {
-  generateButtonToSwitch();
-}
-
-*/
-
 export const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 function generateSideButtons(isWindowed) {
   if(isWindowed && fileFetcher.getURLParams().get("windowed") !== null)
     return;
   
-  document.body.innerHTML += `<div id="control-buttons-container" class="overlayed"><div><button id="toggleButton" onclick=handleToggleMenu> Toggle Menu </button></div><div><button id="toggleTheme"> Toggle Theme </button></div></div>`;
+  document.body.innerHTML += `<div id="control-buttons-container" class="overlayed"><div><button id="toggleButton" onclick=handleToggleMenu><img alt="Toggle Menu" class="small_pic buttons" src="${fileFetcher.makeLinkIndependent("image/SmallPictures/menu.png")}" </button></div><div><button id="toggleTheme"><img alt="Toggle Theme" class="small_pic buttons" src="${fileFetcher.makeLinkIndependent(localStorage.getItem("isSwappedTheme") == "true" ? "image/SmallPictures/sun.png" : "image/SmallPictures/moon.png")}" id="themeImage"></button></div></div>`;
   
   document.getElementById("toggleButton").onclick = () => {
     document.getElementById('sideMenu').style.display = (document.getElementById('sideMenu').style.display === "block") ? "none" : "block";
@@ -150,6 +52,8 @@ async function transferTheme() {
   
   let step = parseFloat(rootGet.getPropertyValue('--step'));
   
+  document.getElementById("themeImage").src = fileFetcher.makeLinkIndependent(isSwappedTheme ? "image/SmallPictures/moon.png" : "image/SmallPictures/sun.png");
+  
   for(let x = 0.0; x < 1.0; x += step) {
     
     rootSet.setProperty('--back', rgbToHex(Math.floor(fitValues(backOg.r, textOg.r, x)),Math.floor(fitValues(backOg.g, textOg.g, x)),Math.floor(fitValues(backOg.b, textOg.b, x))));
@@ -192,12 +96,3 @@ export async function setUpSideButtons(isWindowed) {
   setUpThemeSwitchButton(isWindowed);
 }
 
-  
-//let root = document.documentElement;
-//for(let i = 0; i < themesJson.length; i++) {
-//  if(themesJson[i].name.includes(themeArg)) {
-//    root.style.setProperty('--back', themesJson[i].main);
-//    root.style.setProperty('--mcol', themesJson[i].semi);
-//    root.style.setProperty('--text', themesJson[i].text);
-//  }
-//}
